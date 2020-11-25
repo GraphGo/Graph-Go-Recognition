86da04301636e8d7bf27c670ce04cd6fd1ab8647
Jinhao <jinhaoliu18@hotmail.com>
2020-11-25 07:56:27 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(cors())
+app.options('*', cors())
+app.use(bodyParser.json());
+app.set('trust proxy', true);
+
+const routes = require('./routes');
+app.use('/', routes);
+
+app.use(express.json());
+app.listen(5000, () => console.log('Server started'));
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    if (!req.body) {
+        return res.status(400).send({
+            message: "No data is passed in!"
+        });
+    }
+    // call recognition function
+    res.send({result: [1, 5, 2, 3, 4]});
+})
